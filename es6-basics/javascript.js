c6587a7fe5f5ec4ee2ca13273ee1abe541b3a088
Ntare22 <jim.musika2016@kepler.org>
2019-12-11 23:16:53 +0200

    learned about some helpers

--- es6-basics/javascript.js
+++ es6-basics/javascript.js
@@ -164,3 +164,61 @@
 // })
 
 // console.log(carPrices)
+
+// const products = [
+//   { name: 'ipad', category: 'devices', number: 20, price: 200 },
+//   { name: 'Sony 3D', category: 'TV', number: 0, price: 400 },
+//   { name: 'E256', category: 'Blender', number: 3, price: 50 },
+//   { name: 'Super Vision', category: 'TV', number: 7, price: 500 },
+//   { name: 'Iphone 7', category: 'phones', number: 40, price: 2 },
+// ];
+
+// const result = products.filter((product) => {
+//   return product.category === 'TV' && product.number > 0
+// })
+
+// console.log(result);
+
+// const result = products.filter((product) => {
+//   return product.category === 'TV'
+// })
+
+// console.log(result);
+
+// const brands = [
+//   { name: 'Apple', id: 1 },
+//   { name: 'HP', id: 2 },
+//   { name: 'Samsung', id: 3 },
+//   { name: 'Samsung', id: 4 }
+// ];
+
+// const result = brands.find((brand) => {
+//   return brand.name === 'Samsung'
+// })
+
+// console.log(result)
+
+const cars = [
+  { brand: "Ford", price: 500, available: 2, type: "Sport car" },
+  { brand: "Nissan", price: 200, available: 5, type: "Wagon" },
+  { brand: "Chevy", price: 300, available: 4, type: "Sport car" },
+  { brand: "Porshe", price: 100, available: 1, type: "Urban" },
+];
+
+const getResults = (price, type) => {
+  return cars.find((car) => {
+    return car.price < price && car.available > 0 && car.type === type;
+  })
+}
+
+document.querySelector('.search').addEventListener('click', () => {
+  let price = parseInt(document.querySelector('#price').value);
+  let type = document.querySelector('#type').value;
+
+  let results = getResults(price, type);
+  if (results) {
+    console.log(`Found ${results.brand} for $${results.price}`)
+  } else {
+    console.log(`No cars available filling those criteria`)
+  }
+})
