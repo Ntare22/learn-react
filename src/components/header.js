25901e364dbfd1e07cb3c0a0910d8b7ea201534a
Ntare22 <jim.musika2016@kepler.org>
2019-12-16 23:18:51 +0200

    learning about creating react components

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
+
+
 class Header extends Component {
+
+  state = {
+    keywords: ''
+  }
+
+  inputChangeHander = (event) => {
+    // console.log(event.target.value)
+    this.setState({
+      keywords: event.target.value
+    })
+  }
+
   render() {
     return (
       <header>
         <div className='logo'>Logo</div>
-        <input type="text" />
+        <input type="text" onChange={this.inputChangeHander}/>
       </header>
     )
   }
---
+++ src/components/news_list.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NewsList = (props) => {
+  console.log(props)
+  return (
+    <div>News List</div>
+  )
+}
+
+export default NewsList;
